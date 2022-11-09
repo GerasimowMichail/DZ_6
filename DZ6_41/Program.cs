e2ae143e29736aeebc6dcb0821e1eef71c705cbf
Mischa <GerasimowMichail@yandex.ru>
2022-11-09 23:01:52 +0300

    Обязательные задачи решены

---
+++ DZ6_41/Program.cs
@@ -0,0 +1,53 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите элементы массива: ");
+
+int [] massiv=new int[size];
+
+int[] array = FillArray(size);
+Console.WriteLine("Введенный массив: ");
+PrintArray(array);
+
+Console.WriteLine();
+// CountElem(array);
+Console.WriteLine($"Количество элементов массива больше 0 равно {CountElem(array)}");
+
+//формируем массив элементов с клавиатуры 
+
+int [] FillArray(int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"Введите {i} элемент массива ");
+        massiv[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return massiv;
+}
+
+//Вывод элементов массива на экран
+
+void PrintArray(int [] array)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Console.Write($"{array[j]}  ");
+    }     
+}
+
+
+//Считаем количество элементов больше 0
+
+int CountElem(int [] array)
+{
+    int count=0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i]>0) count=count+1;
+    }
+    return count;
+
+}
---
+++ DZ6_43/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
+//  задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите к1: ");
+float k1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите к2: ");
+float k2= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b1: ");
+float b1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+float b2= Convert.ToInt32(Console.ReadLine());
+
+float x=0;
+float y=0;
+if(k1==k2 && b1==b2) 
+{
+    Console.WriteLine("Прямые совпадают");
+    
+}
+else if (k1==k2 ) 
+{
+    Console.WriteLine("Прямые паралельны");
+    
+}
+
+
+
+//Поиск точки пересечения
+float [] InterSecktionY1Y2(float k1, float k2, float b1, float b2)
+{
+    float [] interxy=new float[2];
+    {    
+    interxy[0]=(b2-b1)/(k1-k2);
+    interxy[1]=k2*(b2-b1)/(k1-k2)+b2;
+    }    
+    return interxy;
+}
+
+//Вывод элементов массива на экран
+
+void PrintArray(float [] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]}  ");
+    }     
+}
+
+ if (k1!=k2 && b1!=b2 ) 
+{
+{
+float [] array = InterSecktionY1Y2(k1, k2, b1, b2);
+Console.WriteLine($"координаты пересечения x= {array[0]}   y= {array[1]}:  ");
+}
+}
+
